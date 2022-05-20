976a395fb6c1100d7d06cfbc235d05575acf4f22
Punnamanikumar <punnamanikumar@gmail.com>
2022-05-20 17:20:35 +0530

    completed

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Header from "./Components/header/Header";
+import Body1 from "./Components/body/Body1";
+import Body2 from "./Components/body/Body2";
+import Body3 from "./Components/body/Body3";
+import Body4 from "./Components/body/Body4";
+import Footer from "./Components/footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body1 />
+      <Body2 />
+      <Body3 />
+      <Body4 />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
